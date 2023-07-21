4ea9e20f727dc045b0fe46d23cb63e2ebf076bd3
wyj_github <www15294729715@outlook.com>
2023-07-21 19:37:26 +0800

    构建

---
+++ assets/index.a7168ca8.js
@@ -0,0 +1,1 @@
+import{c as e,d as t,e as s}from"./index.059b74ae.js";const o={style:{height:"65vh"}},_=s("h1",null,"\u5173\u4E8E",-1),c=[_],h={__name:"index",setup(n){return(a,r)=>(e(),t("div",o,c))}};export{h as default};
---
+++ assets/mixin.d5ce98a8.js
@@ -0,0 +1,1 @@
+function t(){return{funType:a=>{let e="";switch(a){case 1:e="\u521D\u7EA7";break;case 2:e="\u4E2D\u7EA7";break;case 3:e="\u9AD8\u7EA7";break;default:e=""}return e}}}export{t as m};
